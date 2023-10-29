8213f2a5738af26bd02dd18da568b011f3a98dcd
Anastasia Karachova <nastiakarchova@gmail.com>
2023-10-29 23:56:20 +0200

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#635FC7',
+        primaryLight: '#A8A4FF',
+        black: '#000112',
+        veryDarkGray: '#20212C',
+        darkGray: '#2B2C37',
+        mediumGray: '#828FA3',
+        lightGray: '#F4F7FD',
+        red: '#EA5555',
+        redLight: '#FF9898',
+        linesLight: '#E4EBFA',
+        linesDark: '#3E3F4E',
+      },
+    },
   },
   plugins: [],
 }
